ab5a113bad19e094aacc40b536834a71b9c3559f
Angel <ngeljames2409@gmail.com>
2020-12-12 21:40:30 +0530

    final commit

---
+++ customvalidatorsdemo/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace customvalidatorsdemo.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        
+
+       
+    }
+}
